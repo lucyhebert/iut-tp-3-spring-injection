91eaa7a5fa97cd7f7170251ac04f08fa26920d96
Jean Detoeuf <thebignet@gmail.com>
2017-05-01 12:30:07 +0200

    test 2

--- src/test/java/edu/lyon1/Tp3ApplicationTests.java
+++ src/test/java/edu/lyon1/Tp3ApplicationTests.java
@@ -5,6 +5,7 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Service;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -16,17 +17,27 @@ import static org.assertj.core.api.Assertions.assertThat;
 @SpringBootTest
 public class Tp3ApplicationTests {
 
-	@Autowired
-	private ApplicationContext applicationContext;
+    @Autowired
+    private ApplicationContext applicationContext;
 
-	@Test
-	public void contextLoads() {
-	}
+    @Test
+    public void contextLoads() {
+    }
 
-	@Test
-	public void shouldContainAService(){
-		Map<String, Object> beansWithServiceAnnotation = applicationContext.getBeansWithAnnotation(Service.class);
-		assertThat(beansWithServiceAnnotation).isNotEmpty();
-	}
+    @Test
+    public void shouldContainAService() {
+        Map<String, Object> beansWithServiceAnnotation = applicationContext.getBeansWithAnnotation(Service.class);
+        assertThat(beansWithServiceAnnotation).isNotEmpty();
+    }
+
+    @Test
+    public void shouldContainAConfiguration() {
+        Map<String, Object> beansWithConfigurationAnnotation = applicationContext.getBeansWithAnnotation(Configuration.class);
+        boolean hasConfiguration = beansWithConfigurationAnnotation.values().stream()
+                .map(bean -> bean.getClass().getName())
+                .filter(name -> name.startsWith("edu.lyon1"))
+                .anyMatch(name -> !name.contains("Tp3Application"));
+        assertThat(hasConfiguration).isTrue();
+    }
 
 }
