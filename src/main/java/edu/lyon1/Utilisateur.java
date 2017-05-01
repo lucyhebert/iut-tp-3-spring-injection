0129256eaf16b87de64b864912eba64f27a648b7
Jean Detoeuf <thebignet@gmail.com>
2017-05-01 13:50:37 +0200

    correction de l'étape 5

---
+++ src/main/java/edu/lyon1/Utilisateur.java
@@ -0,0 +1,22 @@
+package edu.lyon1;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Utilisateur {
+    private final String prenom;
+    private final String nom;
+
+    public Utilisateur(String prenom, String nom) {
+        this.prenom = prenom;
+        this.nom = nom;
+    }
+
+    public String getPrenom() {
+        return prenom;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+}
