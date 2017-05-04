a2fdc39b7128cef614b0f2ff606fe124bef14512
lucyhebert <lucyhebert@free.fr>
2017-05-04 11:55:07 +0200

    Implémente étape 10 (non testé)

--- src/main/java/edu/lyon1/Config.java
+++ src/main/java/edu/lyon1/Config.java
@@ -3,6 +3,9 @@ package edu.lyon1;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.util.ArrayList;
+import java.util.Collection;
+
 /**
  * Created by p1614402 on 04/05/2017.
  */
@@ -14,4 +17,17 @@ public class Config {
     public Utilisateur utilisateur() {
         return new Utilisateur();
     }
+
+    @Bean(name="utilisateurs")
+    public Collection<Utilisateur> listeUtilisateurs() {
+        Collection<Utilisateur> users = new ArrayList<>();
+
+        users.add(new Utilisateur("Homer", "Simpson"));
+        users.add(new Utilisateur("John", "Doe"));
+        users.add(new Utilisateur("Blah", "Blah"));
+
+        return users;
+    }
+
+
 }
--- src/main/java/edu/lyon1/MyService.java
+++ src/main/java/edu/lyon1/MyService.java
@@ -3,6 +3,8 @@ package edu.lyon1;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Collection;
+
 /**
  * Created by p1614402 on 04/05/2017.
  */
@@ -13,11 +15,20 @@ public class MyService {
     @Autowired
     private Utilisateur utilisateur;
 
+    @Autowired
+    private Collection<Utilisateur> utilisateurs;
+
     public MyService(Utilisateur user) {
+
         this.utilisateur = user;
     }
 
     public boolean checkUser(Utilisateur user) {
+
         return utilisateur.equals(user);
+    }
+
+    public boolean checkUserInList(Utilisateur user) {
+        return utilisateurs.contains(user);
     }
 }
